9d583a719ace4497e90537cca34c493b18489f09
jbinde02 <jbinde02@rams.shepherd.edu>
2021-04-20 14:54:42 -0400

    Added JSON input file to resource folder. Started setting up model class.

--- src/main/java/VendMacModel.java
+++ src/main/java/VendMacModel.java
@@ -1,5 +1,13 @@
 public class VendMacModel {
+    int rows, columns;
+
     public VendMacModel(){
         System.out.println("Model Created");
     }
+}
+
+class VendMacItem{
+    String name;
+    int amount;
+    double cost;
 }
